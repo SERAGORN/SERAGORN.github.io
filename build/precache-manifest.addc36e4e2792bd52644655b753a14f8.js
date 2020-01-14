93157518fa2e5cb7beba0b99f4bc3fc46362ccba
sgtserj <sgtserj94@gmail.com>
2020-01-14 17:57:15 +0900

    set my page

---
+++ build/precache-manifest.addc36e4e2792bd52644655b753a14f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "555bb161033729b5aadcbe03b2da8abc",
+    "url": "/SERAGORN.github.io/index.html"
+  },
+  {
+    "revision": "1a9fbbec1582bd414670",
+    "url": "/SERAGORN.github.io/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2e27af7627b1d928ebc4",
+    "url": "/SERAGORN.github.io/static/js/2.0671d891.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/SERAGORN.github.io/static/js/2.0671d891.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1a9fbbec1582bd414670",
+    "url": "/SERAGORN.github.io/static/js/main.5bd1b495.chunk.js"
+  },
+  {
+    "revision": "1ea87de54c4a4f99b086",
+    "url": "/SERAGORN.github.io/static/js/runtime-main.a9e3604d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SERAGORN.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ src/pages/main/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <div>
+        Wellcome for my sound list
+    </div>
+  );
+}
+
+export default Main;

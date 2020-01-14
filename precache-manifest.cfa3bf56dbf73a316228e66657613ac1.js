a576aad8277d6f4dbc4930369391daffbd400a42
sgtserj <sgtserj94@gmail.com>
2020-01-14 17:35:33 +0900

    add build

---
+++ precache-manifest.cfa3bf56dbf73a316228e66657613ac1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63a4bd03fdb875fc6ff97ec9742a563c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8e04b4c1c42f40e5a89",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "85d4dcfa03a41665f58f",
+    "url": "/static/js/2.bdce0e11.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.bdce0e11.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d8e04b4c1c42f40e5a89",
+    "url": "/static/js/main.c7f79435.chunk.js"
+  },
+  {
+    "revision": "da692fd86d9ffa358f20",
+    "url": "/static/js/runtime-main.3cdf1b2a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
